41816a3b96b65eb4770599c3f6e67bb31c3f37b4
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2021-03-18 11:24:40 -0500

    Create link-files.sh

---
+++ link-files.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd files
+cp -R boot/* /boot/
+for file in $(find etc/* -type f); do rm /$file; ln -sf $PWD/$file /$file; done
+for file in $(find lib/systemd/system/* -type f); do rm /$file; ln -sf $PWD/$file /$file; done
+for file in $(find root/.b* -type f); do rm /$file; ln -sf $PWD/$file /$file; done
+for file in $(find root/.b* -type f); do rm /$file; ln -sf $PWD/$file /etc/skel/${file/root/}; done
+for file in $(find root/.b* -type f); do rm /$file; ln -sf $PWD/$file /home/pi/${file/root/}; done
+for file in $(find root/.b* -type f); do rm /$file; ln -sf $PWD/$file /home/vpn/${file/root/}; done
+for file in $(find sbin/* -type f); do  rm /$file; ln -sf $PWD/$file /$file; done
+mkdir -p /usr/local/bin/helpers
+for file in $(find usr/* -type f); do  rm /$file; ln -sf $PWD/$file /$file; done
