08acbd893fb8b9120339f1f9152fe9921014dae8
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2021-03-25 16:06:49 -0500

    Update docker-helper.sh

--- files/usr/local/bin/helpers/docker-helper.sh
+++ files/usr/local/bin/helpers/docker-helper.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 ! mount | grep " /var/lib/docker " >& /dev/null && mount LABEL=DOCKER /var/lib/docker
+! test -d /var/lib/docker/data && mkdir -p /var/lib/docker/data
 ! mount | grep " /opt/docker-data " >& /dev/null && mount --bind /var/lib/docker/data /opt/docker-data
 exit 0
