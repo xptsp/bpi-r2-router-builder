f3dcdfb2fcc6edfdc50abd7714fd377534ab641c
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2021-03-25 16:02:10 -0500

    Changes

--- files/usr/local/bin/helpers/docker-helper.sh
+++ files/usr/local/bin/helpers/docker-helper.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-DEV=/dev/sda2
-[[ -f /etc/default/docker-helper ]] && source /etc/default/docker-helper
-[[ -z "${DEV}" || ! -e ${DEV} ]] && exit 0
-if mount | grep "${DEV} " >& /dev/null; then
-	if ! mount | grep " /var/lib/docker "; then
-		DIR=$(mount | grep ${DEV} | cut -d" " -f 3)
-		[[ ! -d ${DIR}/docker ]] && cp -R /var/lib/docker ${DIR}/docker
-		mount --bind ${DIR}/docker /var/lib/docker
-	fi
-fi
+! mount | grep " /var/lib/docker " >& /dev/null && mount LABEL=DOCKER /var/lib/docker
+! mount | grep " /opt/docker-data " >& /dev/null && mount --bind /var/lib/docker/data /opt/docker-data
 exit 0
