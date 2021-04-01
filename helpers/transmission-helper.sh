050e0caaedb3dcba10757d53f5335968b8174beb
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2021-04-01 07:12:24 -0500

    Updates

--- helpers/transmission-helper.sh
+++ helpers/transmission-helper.sh
@@ -4,12 +4,13 @@ if [[ "${UID}" -ne 0 ]]; then
 	exit $?
 fi
 FILE=/var/run/transmission-daemon.rule
+JSON=/etc/transmission-daemon/settings.json
 if [[ "$1" == "start" ]]; then
 	# Set the WebUI credentials and port for the transmission-daemon:
 	source /etc/default/transmission-autoremove
-	sed -i "s|\"rpc-username\": \".*\",|\"rpc-username\": \"$USER\",|g" /etc/transmission-daemon/settings.json
-	sed -i "s|\"rpc-password\": \".*\",|\"rpc-password\": \"$PASS\",|g" /etc/transmission-daemon/settings.json
-	sed -i "s|\"rpc-port\": \".*\",|\"rpc-port\": $PORT,|g" /etc/transmission-daemon/settings.json
+	sed -i "s|\"rpc-username\": \".*\",|\"rpc-username\": \"$USER\",|g" ${JSON}
+	sed -i "s|\"rpc-password\": \".*\",|\"rpc-password\": \"$PASS\",|g" ${JSON}
+	sed -i "s|\"rpc-port\": \".*\",|\"rpc-port\": $PORT,|g" ${JSON}
 
 	# Allow WebUI port to always be open for transmission-daemon:
 	RULE="iptables -D OUTPUT ! -o lo -p tcp --sport $PORT -m owner --uid-owner vpn -j ACCEPT"
--- upgrade.sh
+++ upgrade.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 RED='\033[1;31m'
 GREEN='\033[1;32m'
+BLUE='\033[1;34m'
 NC='\033[0m'
 
 # Files to copy only:
 COPY_ONLY=(
-	etc/network/interface.d/
-	etc/network/dnsmasq.d/
-	etc/fstab
-	etc/transmission-daemon/settings.json
-	etc/default/
-	etc/overlayRoot.conf
+	/etc/network/interfaces.d/
+	/etc/network/dnsmasq.d/
+	/etc/fstab
+	/etc/transmission-daemon/settings.json
+	/etc/default/
+	/etc/overlayRoot.conf
 )
 
 function replace()
@@ -18,20 +19,27 @@ function replace()
 	DEST=/${2:-"$1"}
 	COPY=false
 	SRC=$(echo ${PWD}/$1 | sed "s|/ro/|/|g")
-	for cfile in ${COPY_ONLY[@]}; do if [[ "$1" =~ ^${cfile} ]]; then COPY=true; fi; done
+	for MATCH in ${COPY_ONLY[@]}; do 
+		[[ "${DEST:0:${#MATCH}}" == "${MATCH}" ]] && COPY=true
+	done
+	mkdir -p $(dirname ${DEST})
 	if [[ "$COPY" == "true" ]]; then
 		echo -e -n "Copying ${BLUE}${SRC}${NC}... "
-		if ! cp ${SRC} /${DEST}; then
+		if ! cp ${SRC} ${DEST}; then
 			echo -e "${RED}FAIL!${NC}"
 		else
 			echo -e "${GREEN}Success!${NC}"
 		fi
 	else
-		INFO=($(ls -l /${DEST}))
-		if [[ ! "${INFO[-1]}" =~ ^$(dirname $0) ]]; then
-			rm /${DEST}
+		unset INFO
+		if test -e ${DEST}; then
+			INFO=($(ls -l /${DEST}))
+			INFO=${INFO[-1]}
+		fi
+		if [[ ! "${INFO}" == "$SRC" ]]; then
+			test -f ${DEST} && rm ${DEST}
 			echo -e -n "Linking ${BLUE}${SRC}${NC}... "
-			if ! ln -sf ${SRC} /${DEST}; then
+			if ! ln -sf ${SRC} ${DEST}; then
 				echo -e "${RED}FAIL!${NC}"
 			else
 				echo -e "${GREEN}Success!${NC}"
@@ -41,7 +49,7 @@ function replace()
 }
 
 cd $(dirname $0)/files
-cp -R boot/* boot/
+mount | grep " /boot " >& /dev/null && cp -R boot/* boot/
 for file in $(find etc/* -type f); do replace $file; done
 for file in $(find lib/systemd/system/* -type d); do replace $file; done
 for file in $(find root/.b* -type f); do
@@ -54,3 +62,4 @@ for file in $(find sbin/* -type f); do replace $file; done
 test -d /usr/local/bin/helpers || mkdir -p /usr/local/bin/helpers
 for file in $(find usr/* -type f); do replace $file; done
 replace /var/www/router
+
