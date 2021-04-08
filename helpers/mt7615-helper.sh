fe7ae038216b205aaf1fab33ecf649cae71e778c
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2021-04-08 14:54:29 -0500

    Updates

--- helpers/mt7615-helper.sh
+++ helpers/mt7615-helper.sh
@@ -1,46 +1,7 @@
 #!/bin/bash
-SPC="--------"
-
-function runCMD()
-{
-	echo "$SPC CMD: ${@} $SPC"
-	${@}
-	echo -e "$SPC\n"
-}
-
-function setInterface()
-{
-	echo "Network interface: ${1}"
-	runCMD "ip link set ${1} down"
-
-	# Change MAC address:
-	MAC=$(ifconfig ${1} | grep ether | awk '{print $2}')
-	MAC=${MAC:0:${#MAC}-1}0
-	runCMD "ifconfig ${1} hw ether ${MAC}"
-	sed -i "s|bssid=.*|bssid=${MAC}|g" /etc/hostapd/${1}.conf
-
-	# Rename network interface:
-	SUB=10
-	NEW=mt_24g
-	[[ "${1}" == "rename"* ]] && NEW=mt_50g && SUB=11
-	runCMD "ip link set ${1} name ${NEW}"
-	runCMD "ip link set ${NEW} up"
-	runCMD "ifconfig ${NEW} 192.168.${SUB}.1"
-}
 
 # Enable DBDC on any MT76xx wifi card that supports it:
 for file in /sys/kernel/debug/ieee80211/*; do 
         test -e $file/mt76/dbdc && echo 1 > $file/mt76/dbdc
 done
-
-# Rename the interfaces of the MT7615 card:
-sleep 1
-PCI=$(lspci | grep MEDIATEK | grep 7615 | cut -d" " -f 1)
-if [[ ! -z "${PCI}" ]]; then
-	cd /sys/class/net
-	IFACES=($(ls -l | grep "${PCI}" | awk '{print $9}' | grep -v "^mt_"))
-	for IFACE in ${IFACES[@]}; do
-		setInterface ${IFACE} >& /var/run/mt7615-helper.${IFACE}.log
-	done
-fi
 exit 0
--- helpers/transmission-helper.sh
+++ helpers/transmission-helper.sh
@@ -8,9 +8,9 @@ JSON=/etc/transmission-daemon/settings.json
 if [[ "$1" == "start" ]]; then
 	# Set the WebUI credentials and port for the transmission-daemon:
 	source /etc/default/transmission-autoremove
-	sed -i "s|\"rpc-username\": \".*\",|\"rpc-username\": \"$USER\",|g" ${JSON}
-	sed -i "s|\"rpc-password\": \".*\",|\"rpc-password\": \"$PASS\",|g" ${JSON}
-	sed -i "s|\"rpc-port\": \".*\",|\"rpc-port\": $PORT,|g" ${JSON}
+	sed -i "s|\"rpc-username\": \".*\",|\"rpc-username\": \"${USER}\",|g" ${JSON}
+	sed -i "s|\"rpc-password\": \".*\",|\"rpc-password\": \"${PASS}\",|g" ${JSON}
+	sed -i "s|\"rpc-port\": \".*\",|\"rpc-port\": ${PORT},|g" ${JSON}
 
 	# Allow WebUI port to always be open for transmission-daemon:
 	RULE="iptables -D OUTPUT ! -o lo -p tcp --sport $PORT -m owner --uid-owner vpn -j ACCEPT"
