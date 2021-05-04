6a7fa3c83dbd4561cc9e3757c593eb1a51b14cef
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2021-05-04 16:18:31 -0500

    Update site.js

--- router/js/site.js
+++ router/js/site.js
@@ -1,6 +1,33 @@
 function get_Basic_Data()
 {
 	$.getJSON("/api/status", function(results) {
+		// Update internet connectivity status:
+		if (results.wan_status == "Online")
+			$("#connectivity-div").removeClass("bg-danger");
+		else
+			$("#connectivity-div").addClass("bg-danger");
+		$("#connectivity-spinner").remove();
+		$("#connectivity-text").html(results.wan_status);
+
+		// Update number of attached devices:
+		$("#devices-spinner").remove();
+		$("#num_of_devices").html(results.lan_count);
+
+		// Update number of mounted USB devices:
+		if (results.usb_count == 1)
+			$("#usb-sharing").html(results.usb_count.toString() + " Device");
+		else if (results.usb_count >= 0)
+			$("#usb-sharing").html(results.usb_count.toString() + " Devices");
+		else
+			$("#usb-sharing").html("Disabled");
+
+		// Update PiHole statistics:
+		$("#unique_clients").html(results.unique_clients);
+		$("#dns_queries_today").html(results.dns_queries_today);
+		$("#ads_blocked_today").html(results.ads_blocked_today);
+		$("#ads_percentage_today").html(results.ads_percentage_today);
+		$("#domains_being_blocked").html(results.domains_being_blocked);
+
 		// Update system temperature:
 		$("#temp").html(results.temp);
 		if (results.temp > 60)
@@ -16,34 +43,5 @@ function get_Basic_Data()
 		// Update server uptime and local time:
 		$("#system_uptime").html(results.system_uptime);
 		$("#server_time").html(results.server_time);
-
-		// Update system temperature:
-		if (results.wan_status == "Online")
-			$("#connectivity-div").removeClass("bg-danger");
-		else
-			$("#connectivity-div").addClass("bg-danger");
-
-		// Update PiHole statistics:
-		$("#unique_clients").html(results.unique_clients);
-		$("#dns_queries_today").html(results.dns_queries_today);
-		$("#ads_blocked_today").html(results.ads_blocked_today);
-		$("#ads_percentage_today").html(results.ads_percentage_today);
-		$("#domains_being_blocked").html(results.domains_being_blocked);
-
-		// Update the internet connectivity status:
-		$("#connectivity-spinner").remove();
-		$("#connectivity-text").html(results.wan_status);
-
-		// Update number of attached devices:
-		$("#devices-spinner").remove();
-		$("#num_of_devices").html(results.lan_count);
-
-		// Update number of mounted USB devices:
-		if (results.usb_count == 1)
-			$("#usb-sharing").html(results.usb_count.toString() + " Device");
-		else if (results.usb_count >= 0)
-			$("#usb-sharing").html(results.usb_count.toString() + " Devices");
-		else
-			$("#usb-sharing").html("Disabled");
 	});
 }
