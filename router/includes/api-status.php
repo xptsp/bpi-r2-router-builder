2b4946dd4aa80ca43074c7ef7d17ce2568c4f993
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2021-05-04 15:47:36 -0500

    WebUI  Updates

--- router/includes/api-status.php
+++ router/includes/api-status.php
@@ -1,6 +1,6 @@
 <?php
 require_once('subs-detailed.php');
-#header('Content-type: application/json');
+header('Content-type: application/json');
 
 ##########################################################################################
 # Get information for the AJAX request:
@@ -10,10 +10,9 @@ $temp = number_format((float) @file_get_contents('/sys/devices/virtual/thermal/t
 $pihole = @json_decode( @file_get_contents( "http://pi.hole/admin/api.php?summary" ) );
 
 ##########################################################################################
-# Split each line of the results of the "arp" command into array elements:
+# Insert hardware statistics information into array:
 ##########################################################################################
 $arr = array(
-	'count' => 0,
 	'load0' => number_format((float)$load[0], 2),
 	'load1' => number_format((float)$load[1], 2),
 	'load2' => number_format((float)$load[2], 2),
@@ -21,8 +20,15 @@ $arr = array(
 	'temp_icon' => 'fa-thermometer-' . ($temp > 70 ? 'full' : ($temp > 60 ? 'three-quarters' : ($temp > 50 ? 'half' : ($temp > 40 ? 'quarter' : 'empty')))),
 	'system_uptime' => system_uptime(),
 	'server_time' => date('Y-m-d H:i:s'),
-	'devices' => array(),
+	'lan_devices' => array(),
+	'lan_count' => 0,
+	'usb_devices' => array(),
+	'usb_count' => 0,
 );
+
+##########################################################################################
+# Insert Pi-Hole statistics information into array:
+##########################################################################################
 if (isset($pihole->unique_clients))
 	$arr['unique_clients'] = $pihole->unique_clients;
 if (isset($pihole->dns_queries_today))
@@ -44,19 +50,34 @@ else
 	$arr['wan_status'] = strpos(@shell_exec('ping -c 1 -W 1 8.8.8.8'), '1 received') > 0 ? 'Online' : 'Offline';
 
 ##########################################################################################
-# Split each line of the dnsmasq.leases file and place into appropriate element:
+# Parse the dnsmasq.leases file into the "devices" element of the array:
 ##########################################################################################
 foreach (explode("\n", trim(@file_get_contents("/var/lib/misc/dnsmasq.leases"))) as $num => $line)
 {
 	$temp = explode(" ", preg_replace("/\s+/", " ", $line));
-	$arr['devices'][] = array(
+	$arr['lan_devices'][] = array(
 		'lease_expires' => $temp[0],
 		'mac_address' => $temp[1],
 		'ip_address' => $temp[2],
 		'machine_name' => $temp[3],
 	);
 }
-$arr['count'] = count($arr['devices']);
+$arr['lan_count'] = count($arr['lan_devices']);
+
+##########################################################################################
+# Get the number of mounted USB devices:
+##########################################################################################
+foreach (glob('/etc/samba/smb.d/*.conf') as $file)
+{
+	foreach (explode("\n", trim(@file_get_contents($file))) as $line)
+	{
+		if (preg_match("/path=(\/media\/.*)/", $line, $regex))
+			$arr['usb_devices'][basename($file)]['path'] = $regex[1];
+		if (preg_match("/\#mount_dev=(.*)/", $line, $regex))
+			$arr['usb_devices'][basename($file)]['mount_dev'] = $regex[1];
+	}
+}
+$arr['usb_count'] = count($arr['usb_devices']);
 
 ##########################################################################################
 # Output the resulting array:
--- router/js/site.js
+++ router/js/site.js
@@ -1,21 +1,19 @@
 function get_Basic_Data()
 {
 	$.getJSON("/api/status", function(results) {
+		# Update system temperature:
 		$("#temp").html(results.temp);
+
+		# Update system load averages:
 		$("#load0").html(results.load0);
 		$("#load1").html(results.load1);
 		$("#load2").html(results.load2);
+
+		# Update server uptime and local time:
 		$("#system_uptime").html(results.system_uptime);
 		$("#server_time").html(results.server_time);
-		$("#unique_clients").html(results.unique_clients);
-		$("#dns_queries_today").html(results.dns_queries_today);
-		$("#ads_blocked_today").html(results.ads_blocked_today);
-		$("#ads_percentage_today").html(results.ads_percentage_today);
-		$("#domains_being_blocked").html(results.domains_being_blocked);
-		$("#devices-spinner").remove();
-		$("#num_of_devices").html(results.count);
-		$("#connectivity-spinner").remove();
-		$("#connectivity-text").html(results.wan_status);
+
+		# Update system temperature:
 		if (results.wan_status == "Online")
 			$("#connectivity-div").removeClass("bg-danger");
 		else
@@ -24,5 +22,28 @@ function get_Basic_Data()
 			$("#temp-danger").removeClass("invisible");
 		else
 			$("#temp-danger").addClass("invisible");
+
+		# Update PiHole statistics:
+		$("#unique_clients").html(results.unique_clients);
+		$("#dns_queries_today").html(results.dns_queries_today);
+		$("#ads_blocked_today").html(results.ads_blocked_today);
+		$("#ads_percentage_today").html(results.ads_percentage_today);
+		$("#domains_being_blocked").html(results.domains_being_blocked);
+
+		# Update the internet connectivity status:
+		$("#connectivity-spinner").remove();
+		$("#connectivity-text").html(results.wan_status);
+
+		# Update number of attached devices:
+		$("#devices-spinner").remove();
+		$("#num_of_devices").html(results.lan_count);
+
+		# Update number of mounted USB devices:
+		if (results.usb_count == 1)
+			$("#usb-sharing").html(results.usb_count.toString() + " Device");
+		else if (results.usb_count >= 0)
+			$("#usb-sharing").html(results.usb_count.toString() + " Devices");
+		else
+			$("#usb-sharing").html("Disabled");
 	});
 }
