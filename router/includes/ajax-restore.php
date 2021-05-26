6207b82711678fdba760ed2aee90f30ab62f25bb
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2021-05-26 17:24:55 -0500

    Update ajax-restore.php

--- router/includes/ajax-restore.php
+++ router/includes/ajax-restore.php
@@ -1,6 +1,6 @@
 <?php
-$restore = isset($_POST['restore_type']) ? $_POST['restore_type'] : 'fail';
-if (!isset($_POST['sid']) || $_POST['sid'] != strrev(session_id()) || !in_array($restore, array('file', 'factory', 'upload')))
+$mode = isset($_POST['restore_type']) ? $_POST['restore_type'] : 'fail';
+if (!isset($_POST['sid']) || $_POST['sid'] != strrev(session_id()) || !in_array($mode, array('file', 'factory', 'upload')))
 {
 	require_once("404.php");
 	exit;
@@ -9,15 +9,15 @@ if (!isset($_POST['sid']) || $_POST['sid'] != strrev(session_id()) || !in_array(
 ####################################################################################
 # If this is a settings upload, then verify the contents of the upload:
 ####################################################################################
-if ($restore == "upload")
+if ($mode == "upload")
 {
-	if (!isset($_FILES['file']))
+	if (!isset($_FILES['file']['name']))
 		echo "ERROR: No file specified!";
 	else if (strtolower(pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION)) != "cfg")
 		echo 'ERROR: File extension must be "cfg"!';
 	else
 	{
-		@shell_exec("/opt/bpi-r2-router-builder/helpers/router-helper.sh backup remove")
+		@shell_exec("/opt/bpi-r2-router-builder/helpers/router-helper.sh backup remove");
 		if (@move_uploaded_file($_FILES['file']['tmp_name'], '/tmp/bpiwrt.cfg'))
 			echo trim(@shell_exec("/opt/bpi-r2-router-builder/helpers/router-helper.sh backup unpack"));
 		else
@@ -28,7 +28,7 @@ if ($restore == "upload")
 ####################################################################################
 # If this is a factory restore, signal a reformat is needed and return to caller:
 ####################################################################################
-else if ($restore == "factory")
+else if ($mode == "factory")
 	echo explode("\n", trim(@shell_exec("/opt/bpi-r2-router-builder/helpers/router-helper.sh reformat -y")))[0];
 
 ####################################################################################
