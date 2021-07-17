cd5f7312d6386e1882422465b5c7d9ac6481cee3
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2021-07-17 18:48:43 -0500

    Update mt7615-rename.sh

--- helpers/mt7615-rename.sh
+++ helpers/mt7615-rename.sh
@@ -5,14 +5,18 @@ if [[ ! -z "${PCI}" ]]; then
 	cd /sys/class/net
 	IFACES=($(ls -l | grep "${PCI}" | awk '{print $9}' | grep -v "^mt7615_"))
 	for IFACE in ${IFACES[@]}; do
-		# Change MAC address:
-		MAC=$(ifconfig ${IFACE} | grep ether | awk '{print $2}')
 		NEW=mt7615_24g
-		[[ "${IFACE}" == "rename"* ]] && NEW=mt7615_5g
+		IP=192.168.10.1
+		if [[ "${IFACE}" == "rename"* ]]; then
+			NEW=mt7615_5g
+			IP=192.168.20.1
+		fi
 		ifconfig ${IFACE} down
 		ip link set ${IFACE} name ${NEW}
 		ifconfig ${NEW} up
+		ifconfig ${NEW} ${IP} netmask 255.255.255.0
 		systemctl start hostapd@${NEW}
 	done
 fi
+systemctl start pihole-FTL
 exit 0
