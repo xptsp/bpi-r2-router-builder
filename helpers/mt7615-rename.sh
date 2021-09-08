e64710f1964e4e4ba1156d4cb9bbef515981bb9e
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2021-09-07 20:47:52 -0500

    Update mt7615 scripts

--- helpers/mt7615-rename.sh
+++ helpers/mt7615-rename.sh
@@ -7,21 +7,13 @@ if [[ ! -z "${PCI}" ]]; then
 	cd /sys/class/net
 	IFACES=($(ls -l | grep "${PCI}" | awk '{print $9}' | grep -v "^mt${MDL}_"))
 	for IFACE in ${IFACES[@]}; do
-		pushd ${IFACE}
 		NEW=mt${MDL}_24g
-		IP=192.168.10.1
-		if [[ ! -f /sys/kernel/debug/ieee80211/$(basename $(ls -l phy80211 | awk '{print $NF}'))/mt76/dbdc ]]; then
-			NEW=mt${MDL}_5g
-			IP=192.168.20.1
-		fi
-		popd
+		[[ ! -f /sys/kernel/debug/ieee80211/$(basename $(ls -l ${IFACE}/phy80211 | awk '{print $NF}'))/mt76/dbdc ]] && NEW=mt${MDL}_5g
 		ip link set ${IFACE} down
 		ip addr flush dev ${IFACE}
 		ip link set ${IFACE} name ${NEW}
 		iw dev ${NEW} set power_save off
-		iw dev ${NEW} set txpower fixed 2500
 		ip link set ${NEW} up
-		ip addr add ${IP}/24 dev ${NEW}
 		if [[ -f /etc/hostapd/${NEW}.conf ]]; then
 			if [[ "$(cat /etc/hostapd/${NEW}.conf | grep wpa_passphrase | cut -d"=" -f 2)" == "bananapi" ]]; then
 				if [[ -f /boot/wifi.conf ]]; then
@@ -38,5 +30,4 @@ if [[ ! -z "${PCI}" ]]; then
 		fi
 	done
 fi
-[[ -f /usr/local/bin/pihole ]] && pihole restartdns
 exit 0
