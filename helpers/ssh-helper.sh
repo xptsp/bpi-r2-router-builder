df3d240045ff56444cab2d81d86b96735356a6c5
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2021-09-09 15:14:08 -0500

    Reversion back to Debian Buster

--- helpers/ssh-helper.sh
+++ helpers/ssh-helper.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+# Regenerate the missing SSH keys if missing:
 if ! test -e /etc/ssh/ssh_host_rsa_key; then
 	ssh-keygen -N "" -t rsa -f /etc/ssh/ssh_host_rsa_key
 	ssh-keygen -N "" -t ed25519 -f /etc/ssh/ssh_host_ed25519_key
 	ssh-keygen -N "" -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key
 fi
+
+# Turn the blue light on on the side opposite the network ports:
+echo 1 > /sys/class/leds/bpi-r2:pio:blue/brightness
+
 exit 0
