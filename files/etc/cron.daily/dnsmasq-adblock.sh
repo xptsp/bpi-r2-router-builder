41212e2c54bdf2b977c8ed0efee6c78e194459f4
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2021-09-13 18:38:12 -0500

    Removed Adblock

--- files/etc/cron.daily/dnsmasq-adblock.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-wget -q https://github.com/notracking/hosts-blocklists/raw/master/domains.txt -O /etc/dnsmasq.d/domains.txt
-wget -q https://github.com/notracking/hosts-blocklists/raw/master/hostnames.txt -O /etc/dnsmasq.d/hostnames.txt
-systemctl restart dnsmasq
-
