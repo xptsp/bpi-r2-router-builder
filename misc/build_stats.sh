c6e9f280cccc684307f4a10e99fbe15455da0fbe
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2021-09-22 10:32:58 -0500

    Update build_stats.sh

--- misc/build_stats.sh
+++ misc/build_stats.sh
@@ -3,10 +3,10 @@ if [[ "$UID" -ne 1000 ]]; then
 	sudo -u pi -s $0 $@
 	exit
 fi
-DIR=/var/lib/stats
-cd ${DIR}
-virtualenv ${DIR}/env
-source ${DIR}/env/bin/activate
+cd /var/lib/stats
+virtualenv env
+source env/bin/activate
 tar xzvf /opt/stats/ssd1306_python3.tar.gz
 python3 -m pip install --no-index --find-links=./whl psutil Adafruit-SSD1306 Adafruit-BBIO
+rm -rf whl
 
