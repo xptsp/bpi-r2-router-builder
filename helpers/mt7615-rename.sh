7475854b4971d7745c533365dd36973ec65bcfa4
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2021-10-06 19:11:51 -0500

    Wifi Services Update

--- helpers/mt7615-rename.sh
+++ helpers/mt7615-rename.sh
@@ -27,25 +27,17 @@ if [[ ! -z "${PCI}" ]]; then
 		NEW=mt${MDL}_24g
 		[[ ! -f /sys/kernel/debug/ieee80211/$(basename $(ls -l ${IFACE}/phy80211 | awk '{print $NF}'))/mt76/dbdc ]] && NEW=mt${MDL}_5g
 
-		# We need the IP address from the network interfaces file:
-		IP=$(cat /etc/network/interfaces.d/${NEW} 2> /dev/null | grep address | awk '{print $2}')
-
-		# Rename the interface, turn off power save mode, and set new IP address:
-		ip link set ${IFACE} down
-		ip addr flush dev ${IFACE}
-		ip link set ${IFACE} name ${NEW}
-		iw dev ${NEW} set power_save off
-		ip link set ${NEW} up
-		ifconfig ${NEW} ${IP}/24
-
 		# Change interface's password if it is "bananapi", and launch hostapd AP on that interface:
 		if [[ -f /etc/hostapd/${NEW}.conf ]]; then
 			if [[ "$(cat /etc/hostapd/${NEW}.conf | grep wpa_passphrase | cut -d"=" -f 2)" == "bananapi" ]]; then
 				sed -i "s|wpa_passphrase=.*|wpa_passphrase=${WIFI_PASS}|g" /etc/hostapd/${NEW}.conf
 			fi
-			systemctl start hostapd@${NEW}
 		fi
+
+		# Rename the interface and bring the interface up:
+		ip link set ${IFACE} down
+		ip link set ${IFACE} name ${NEW}
+		ip link set ${NEW} up
 	done
 fi
-systemctl restart dnsmasq
 exit 0
