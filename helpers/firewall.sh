210eafc9fb49b77876d2b5cd0fe514156f10ed3c
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2021-11-09 16:06:17 -0600

    Update Firewall Script (Attempt #2)

--- helpers/firewall.sh
+++ helpers/firewall.sh
@@ -1,52 +1,168 @@
-#!/usr/bin/php
+#!/bin/bash
 #############################################################################
-# This helper script takes care of firewall rule creation.
+# This helper script establishes all of the iptables rules required by the
+# WebUI configuration for our router to operate properly.
 #############################################################################
+# Comments starting with "CTA:" and iptables commands from source:
+#	https://javapipe.com/blog/iptables-ddos-protection/
+# Comments starting with "CTB:" and iptables commands from source:
+#	https://offensivesecuritygeek.wordpress.com/2014/06/24/how-to-block-port-scans-using-iptables-only/
+#############################################################################
+if [[ "${UID}" -ne 0 ]]; then
+	sudo $0 $@
+	exit $?
+fi
 
-<?php
-#####################################################################################
-echo "Creating iptables rules file which removes all non-docker rules...\n";
-@shell_exec('(echo "*filter"; /sbin/iptables --list-rules | grep -v -i docker | grep "^-A" | sed "s|-A|-D|g"; echo "COMMIT") > /tmp/iptables.rules');
+# Read the INTERNET from the configuration file:
+[[ -f /etc/default/firewall ]] && source /etc/default/firewall
 
-#####################################################################################
-echo "Removing non-docker iptables rules...\n";
-@shell_exec('/sbin/iptables-restore --noflush < /tmp/iptables.rules');
+#############################################################################
+# START => Initializes the optionless base iptable firewall configuration:
+#############################################################################
+if [[ "$1" == "start" ]]; then
+	#############################################################################
+	# CTB: Flush all the iptables Rules
+	#############################################################################
+	iptables -F
 
-#####################################################################################
-echo "Removing temporary iptables rules file...\n";
-@shell_exec('rm /tmp/iptables.rules');
+	#############################################################################
+	# CTB: Accept loopback input
+	#############################################################################
+	iptables -A INPUT -i lo -p all -j ACCEPT
 
-#####################################################################################
-echo "Putting IPv4 rules into place...\n";
-@shell_exec('/sbin/iptables-restore < /etc/network/iptables.rules');
+	#############################################################################
+	# CTA: Set default policy to DROP for input, output and forwarding:
+	#############################################################################
+	iptables -P INPUT ACCEPT
+	iptables -P FORWARD ACCEPT
+	iptables -P OUTPUT ACCEPT
 
-#####################################################################################
-echo "Putting IPv6 rules into place...\n";
-@shell_exec('cat /etc/network/iptables.rules | grep -v "10.2" | grep -v "192.168" | /sbin/ip6tables-restore');
+	#############################################################################
+	# These are global rules that will always be set!
+	#############################################################################
+	# CTA: Allow masquerading to the wan port:
+	iptables -t nat -A POSTROUTING -o wan -j MASQUERADE
 
-#####################################################################################
-# NOTE: If the "CONFIG_NETFILTER_XT_MATCH_OWNER" isn't enabled during kernel compilation, this line will fail.
-#   Moving it outside the "iptables.rules" file keeps the networking service from failing to initialize.
-echo "Adding rule to prevent user \"vpn\" from sending anything...\n";
-@shell_exec('iptables -A OUTPUT ! -o lo -m owner --uid-owner vpn -j DROP');
+7	# CTA: This rule blocks all packets that are not a SYN packet and don’t
+	# belong to an established TCP connection.
+	iptables -t mangle -A PREROUTING -m conntrack --ctstate INVALID -j DROP
 
-#####################################################################################
-# Define default configuration, then read WebUI configuration into memory:
-echo "Defining default WebUI configuration:\n";
-$rules = array(
-	'disable-ping' => true,
-	'drop-ping' => true,
-);
+	# CTA: This blocks all packets that are new (don’t belong to an established
+	# connection) and don’t use the SYN flag.  This rule is similar to the “Block
+	# Invalid Packets” one, but we found that it catches some packets that the other one doesn’t.
+	iptables -t mangle -A PREROUTING -p tcp ! --syn -m conntrack --ctstate NEW -j DROP
 
-#####################################################################################
-echo "Reading WebUI configuration...\n";
-if (file_exists('/var/opt/router-builder/config.json'))
-	$rules = json_decode(@file_get_contents('/var/opt/router-builder/config.json'), true);
+	# CTA: The above iptables rule blocks new packets (only SYN packets can be
+	# new packets as per the two previous rules) that use a TCP MSS value that
+	# is not common. This helps to block dumb SYN floods.
+	iptables -t mangle -A PREROUTING -p tcp -m conntrack --ctstate NEW -m tcpmss ! --mss 536:65535 -j DROP
 
-#####################################################################################
-# Block pings from the internet?
-if (!empty($rules['disable-ping']))
-{
-	echo "Adding rule to block pings from the internet:\n";
-	@shell_exec('/usr/sbin/iptables -I INPUT -i wan -p icmp --icmp-type echo-request -j ' . (!empty($rules['drop-ping']) ? 'DROP' : 'REJECT'));
-}
+	# CTA: The above ruleset blocks packets that use bogus TCP flags, ie. TCP flags that legitimate packets wouldn’t use.
+	iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,SYN FIN,SYN -j DROP
+	iptables -t mangle -A PREROUTING -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
+	iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,RST FIN,RST -j DROP
+	iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,ACK FIN -j DROP
+	iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,URG URG -j DROP
+	iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,PSH PSH -j DROP
+	iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL NONE -j DROP
+
+	# Redirect incoming port 67 to port 68:
+	iptables -A INPUT -p udp -m udp --sport 67 --dport 68 -j ACCEPT
+
+	# CTB: Droping all invalid packets
+	iptables -A INPUT -m state --state INVALID -j DROP
+	iptables -A FORWARD -m state --state INVALID -j DROP
+	iptables -A OUTPUT -m state --state INVALID -j DROP
+
+	# CTB: for SMURF attack protection
+	iptables -A INPUT -p icmp -m icmp --icmp-type address-mask-request -j DROP
+	iptables -A INPUT -p icmp -m icmp --icmp-type timestamp-request -j DROP
+	iptables -A INPUT -p icmp -m icmp -m limit --limit 1/second -j ACCEPT
+
+	# CTB: flooding of RST packets, smurf attack Rejection
+	iptables -A INPUT -p tcp -m tcp --tcp-flags RST RST -m limit --limit 2/second --limit-burst 2 -j ACCEPT
+
+	#############################################################################
+	# Direct WAN interface to check SERVICES table for further rules:
+	#############################################################################
+	iptables -N SERVICES
+	iptables -A INPUT -i wan -j SERVICES
+
+	#############################################################################
+	# Our "intervention" for miniupnpd to work properly:
+	#############################################################################
+	iptables -N MINIUPNPD
+	iptables -A FORWARD -i wan ! -o wan -j MINIUPNPD
+
+	#############################################################################
+	# Configurable INTERNET chain:
+	#############################################################################
+	iptables -N INTERNET
+	iptables -A INPUT -i wan -j INTERNET
+
+	#############################################################################
+	# Default network configuration:
+	#############################################################################
+	iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+	iptables -A INPUT -i wan -j DROP
+	iptables -A FORWARD -i wan ! -o wan -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+	iptables -A FORWARD -i wan ! -o wan -j DROP
+fi
+
+#############################################################################
+# START/RELOAD => Setup any iptables rules for our WebUI configuration:
+#############################################################################
+if [[ "$1" == "start" || "$1" == "reload" ]]; then
+	#############################################################################
+	# Clear the "INTERNET" iptables chain of rules:
+	#############################################################################
+	iptables -F INTERNET
+
+	#############################################################################
+	# OPTION "disable_port_scan" => Disable ping response from internet
+	#############################################################################
+	[[ "${disable_port_scan:-"Y"}" == "Y" ]] && iptables -A INTERNET -p icmp --icmp-type echo-request -j DROP
+
+	#############################################################################
+	# OPTION "disable_port_scan" => Disable ping response from internet
+	#############################################################################
+	if [[ "${disable_port_scan:-"Y"}" == "Y" ]]; then
+		# CTB: Create a chain port scan and add logging to it with your preferred prefix
+		iptables -N PORTSCAN
+		iptables -A PORTSCAN -j LOG --log-level 4 --log-prefix 'Blocked_scans '
+		iptables -A PORTSCAN -j DROP
+
+		# CTB: Create another chain UDP with custom logging
+		iptables -N UDP
+		iptables -A UDP -j LOG --log-level 4 --log-prefix 'UDP_FLOOD '
+		iptables -A UDP -p udp -m state --state NEW -m recent --set --name UDP_FLOOD
+		iptables -A UDP -j DROP
+
+		# CTB: Continue processing for all connections for ports from 32768 to 61000.  These are mostly
+		# used in ACK and don’t have too many services hosted here.
+		iptables -A INTERNET -p tcp -m tcp --destination-port 32768:61000 -j RETURN
+
+		# CTB: Anyone who previously tried to portscan or UDP flood us are locked out for an entire day.
+		# Their IP’s are stored in a list called ‘PORTSCAN’:
+		iptables -A INTERNET -m recent --name PORTSCAN --rcheck --seconds 86400 -j PORTSCAN
+		iptables -A INTERNET -m recent --name UDP_FLOOD --rcheck --seconds 86400 -j PORTSCAN
+
+		# CTB: Once the day has passed, remove them from the PORTSCAN list:
+		iptables -A INTERNET -m recent --name PORTSCAN --remove
+		iptables -A INTERNET -m recent --name UDP_FLOOD --remove
+
+		# CTB: Anyone who does not match the above rules (open ports) is trying to access a port our sever does not
+		# serve. So, as per design we consider them port scanners and we block them for an entire day
+		# These rules add scanners to the PORTSCAN list, and log the attempt:
+		iptables -A INTERNET -p tcp -m tcp -m recent -m state --state NEW --name PORTSCAN --set -j PORTSCAN
+
+		# CTB: Same for UDP
+		iptables -A INTERNET -p udp -m state --state NEW -m recent --set --name Domainscans
+		iptables -A INTERNET -p udp -m state --state NEW -m recent --rcheck --seconds 5 --hitcount 5 --name Domainscans -j UDP
+	fi
+
+	#############################################################################
+	# OPTION "disable_ident" => Block port 113 (IDENT) from internet
+	#############################################################################
+	[[ "${disable_ident:-"Y"}" == "Y" ]] && iptables -A INTERNET -p tcp --destination-port 113 -j DROP
+fi
