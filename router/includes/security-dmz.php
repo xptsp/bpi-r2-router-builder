d6db3cbe518cb2629742cf377c7ea4199cf24ae2
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2021-11-17 09:01:22 -0600

    Update security-dmz.php

--- router/includes/security-dmz.php
+++ router/includes/security-dmz.php
@@ -4,6 +4,10 @@ $options = parse_file();
 site_menu();
 $src_type = isset($config['dmz_src_type']) ? $config['dmz_src_type'] : 'any';
 $dest_type = isset($config['dmz_dest_type']) ? $config['dmz_dest_type'] : 'addr';
+$default = trim(@shell_exec("ifconfig br0 | grep 'inet ' | awk '{print $2}'"));
+$octet = (int) substr($default, strrpos($default, '.') + 1) + 1;
+$default = substr($default, 0, strrpos($default, '.') + 1) . strval($octet);
+$dest_ip = trim(@shell_exec("arp -i br0 | grep -v Address | sort | head -1 | awk '{print $1}'"));
 echo '
 <div class="card card-primary">
 	<div class="card-header">
@@ -24,14 +28,14 @@ echo '
 						<tr>
 							<td><input type="radio" value="range" id="src_range" name="src_type"> <label for="src_spec"', $src_type == 'range' ? ' checked="checked"' : '', '>IP Range</label</td>
 							<td>		
-								<label for="src_specific"><input id="range_from" type="text"', isset($config['dmz_range_from']) ? ' value="' . $config['dmz_range_from'] . '"' : '', $src_type == "range" ? '' : 'disabled="disabled"', ' class="ip_address form-control" data-inputmask="\'alias\': \'ip\'" data-mask></label> to
-								<label for="src_specific"><input id="range_to" type="text"', isset($config['dmz_range_to']) ? ' value="' . $config['dmz_range_to'] . '"' : '', $src_type == "range" ? '' : 'disabled="disabled"', ' class="form-control" maxlength="3" style="width: 50px" /></label>
+								<label for="src_specific"><input id="range_from" type="text" value="', isset($config['dmz_range_from']) ? $config['dmz_range_from'] : $default, '"', $src_type == "range" ? '' : 'disabled="disabled"', ' class="ip_address form-control" data-inputmask="\'alias\': \'ip\'" data-mask></label> to
+								<label for="src_specific"><input id="range_to" type="text" value="', isset($config['dmz_range_to']) ? $config['dmz_range_to'] : $octet, '"', $src_type == "range" ? '' : 'disabled="disabled"', ' class="form-control" maxlength="3" style="width: 50px" /></label>
 							</td>
 						</tr>
 						<tr>
 							<td><input type="radio" value="mask" id="src_mask" name="src_type"', $src_type == 'mask' ? ' checked="checked"' : '', '> <label for="src_spec">IP Mask</label</td>
 							<td>		
-								<label for="src_mask"><input id="mask_ip" type="text"', isset($config['dmz_mask_ip']) ? ' value="' . $config['dmz_mask_ip'] . '"' : '', $src_type == "mask" ? '' : 'disabled="disabled"', ' class="ip_address form-control" data-inputmask="\'alias\': \'ip\'" data-mask></label>/<label for="src_mask"><input type="text"', $src_type == "mask" ? '' : 'disabled="disabled"', ' id="mask_bits" maxlength="2" style="max-width: 50px" class="form-control" /></label>
+								<label for="src_mask"><input id="mask_ip" type="text" value="', isset($config['dmz_mask_ip']) ? $config['dmz_mask_ip'] : $default, '"', $src_type == "mask" ? '' : 'disabled="disabled"', ' class="ip_address form-control" data-inputmask="\'alias\': \'ip\'" data-mask></label>/<label for="src_mask"><input type="text" value="', !empty($options['dmz_mask_bits']) ? $options['dmz_mask_bits'] : 24, '"', $src_type == "mask" ? '' : 'disabled="disabled"', ' id="mask_bits" maxlength="2" style="max-width: 50px" class="form-control" /></label>
 							</td>
 						</tr>
 					</table>
@@ -43,7 +47,7 @@ echo '
 					<table>
 						<tr>
 							<td width="150px;"><input type="radio" value="addr" id="dest_ip" name="dest_type"', $dest_type == 'addr' ? ' checked="checked"' : '', '> <label for="dest_ip">IP Address</label></td>
-							<td><input id="ip_addr" type="text" class="ip_address form-control" data-inputmask="\'alias\': \'ip\'" data-mask', $dest_type == 'addr' ? '' : ' disabled="disabled"', '></td>
+							<td><input id="ip_addr" type="text" value="', !empty($options['dmz_ip_addr']) ? $options['dmz_ip_addr'] : $dest_ip, '" class="ip_address form-control" data-inputmask="\'alias\': \'ip\'" data-mask', $dest_type == 'addr' ? '' : ' disabled="disabled"', '></td>
 						</tr>
 						<tr>
 							<td><input type="radio" value="mac" id="dest_mac" name="dest_type"', $dest_type == 'mac' ? ' checked="checked"' : '', '> <label for="dest_mac">MAC Address</label></td>
