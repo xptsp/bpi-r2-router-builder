960e0d52bbe49fbe5014e9922609320de4c10d17
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2022-02-04 04:24:01 -0600

    Updated Transmission-daemon configuration

--- helpers/transmission-helper.sh
+++ helpers/transmission-helper.sh
@@ -10,7 +10,6 @@ if [[ "${UID}" -ne 0 ]]; then
 	exit $?
 fi
 
-FILE=/var/run/transmission-daemon.rule
 JSON=/etc/transmission-daemon/settings.json
 if [[ "$1" == "start" ]]; then
 	# Set the WebUI credentials and port for the transmission-daemon:
@@ -18,14 +17,5 @@ if [[ "$1" == "start" ]]; then
 	sed -i "s|\"rpc-username\": \".*\",|\"rpc-username\": \"${USER}\",|g" ${JSON}
 	sed -i "s|\"rpc-password\": \".*\",|\"rpc-password\": \"${PASS}\",|g" ${JSON}
 	sed -i "s|\"rpc-port\": \".*\",|\"rpc-port\": ${PORT},|g" ${JSON}
-
-	# Allow WebUI port to always be open for transmission-daemon:
-	RULE="iptables -D OUTPUT ! -o wan -p tcp --sport $PORT -m owner --uid-owner vpn -j ACCEPT"
-	${RULE/\-D/\-I}
-	echo $RULE > $FILE
-	chmod +x $FILE
-elif [[ "$1" == "stop" ]]; then
-	$FILE
-	rm $FILE
 fi
 exit 0
