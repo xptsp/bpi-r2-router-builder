fda7fd76660d5298eac6d71a7e9c7eecd539d274
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2022-03-02 12:16:24 -0600

    Added helper script to nginx service

---
+++ helpers/nginx-helper.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#############################################################################
+# This helper script takes care of any tasks that should occur before the 
+# SSH service officially starts.  Tasks that occur here should not
+# take very long to execute and should not rely on other services being up
+# and running.
+#############################################################################
+if [[ "${UID}" -ne 0 ]]; then
+	sudo $0 $@
+	exit $?
+fi
+
+#############################################################################
+# Update webserver addresses to match IP address of interface "br0":
+#############################################################################
+cd /etc/nginx/sites-available
+NEW_IP=$(cat /etc/network/interfaces.d/br0 | grep address | awk '{print $2}')
+OLD_IP=$(cat router | grep listen | awk '{print $2}' | cut -d: -f 1)
+[[ "${NEW_IP}" != "${OLD_IP}" ]] && sed -i "s|${OLD_IP}|${NEW_IP}|g" *
+
+#############################################################################
+# Update transmission reverse proxy to match port address specified:
+#############################################################################
+if test -f /etc/default/transmission-daemon; then
+	source /etc/default/transmission-daemon
+	sed -i "s|listen ${NEW_IP}:.*;|listen ${NEW_IP}:${TRANS_PORT};|g" transmission
+fi
+
+#############################################################################
+# Return error code 0 to caller:
+exit 0
+
