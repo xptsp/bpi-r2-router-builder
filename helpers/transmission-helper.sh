3e68aa00320696693ded4517dfb12ccc53a81e2a
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2022-03-02 08:50:34 -0600

    Updated transmission-daemon service

--- helpers/transmission-helper.sh
+++ helpers/transmission-helper.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 #############################################################################
-# This helper script takes care of any tasks that should occur before the 
+# This helper script takes care of any tasks that should occur before the
 # transmission service officially starts.  Tasks that occur here should not
 # take very long to execute and should not rely on other services being up
 # and running.
 #############################################################################
-JSON=/etc/transmission-daemon/settings.json
+if [[ "${UID}" -ne 0 ]]; then
+	sudo $0 $@
+	exit $?
+fi
+
+# Forward all traffic on the peer port to the transmission daemon:
 if [[ "$1" == "start" ]]; then
-	# Set the WebUI credentials and port for the transmission-daemon:
-	source /etc/default/transmission-autoremove
-	sed -i "s|\"rpc-username\": \".*\",|\"rpc-username\": \"${USER}\",|g" ${JSON}
-	sed -i "s|\"rpc-password\": \".*\",|\"rpc-password\": \"${PASS}\",|g" ${JSON}
-	sed -i "s|\"rpc-port\": \".*\",|\"rpc-port\": ${PORT},|g" ${JSON}
+	iptables -I PREROUTING -t nat -i ${TRANS_IFACE:-"wan"} -p tcp --dport ${TRANS_PEERPORT:-"51543"} -j DNAT --to 127.0.0.1:${TRANS_PEERPORT:-"51543"}
+	iptables -I FORWARD -p tcp -d 127.0.0.1 --dport ${TRANS_PEERPORT:-"51543"} -j ACCEPT
+elif [[ "$1" == "stop" ]]; then
+	iptables -D PREROUTING -t nat -i ${TRANS_IFACE:-"wan"} -p tcp --dport ${TRANS_PEERPORT:-"51543"} -j DNAT --to 127.0.0.1:${TRANS_PEERPORT:-"51543"}
+	iptables -D FORWARD -p tcp -d 127.0.0.1 --dport ${TRANS_PEERPORT:-"51543"} -j ACCEPT
 fi
 exit 0
+
