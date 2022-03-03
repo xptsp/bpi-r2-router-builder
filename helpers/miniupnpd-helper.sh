1bf8e88e695973d55f8845fac38a4afabe529733
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2022-03-03 09:04:14 -0600

    Update miniupnpd-helper.sh

--- helpers/miniupnpd-helper.sh
+++ helpers/miniupnpd-helper.sh
@@ -8,7 +8,10 @@ iptables -F MINIUPNPD
 ip6tables -N MINIUPNPD >& /dev/null
 ip6tables -F MINIUPNPD
 
-# If we are starting the service, add a route for 239.0.0.0 to the routing table:
-[[ "$1" == "start" ]] && route add -net 239.0.0.0 netmask 255.0.0.0 $(cat /etc/miniupnpd/miniupnpd.conf | grep "^listening_ip" | head -1 | cut -d= -f 2)
-
+# If we are starting the service, add a route for 239.0.0.0 for each interface that miniupnpd listens on:
+if [[ "$1" == "start" ]]; then
+	for iface in $(cat /etc/miniupnpd/miniupnpd.conf | grep "^listening_ip" | head -1 | cut -d= -f 2); do
+		route add -net 239.0.0.0 netmask 255.0.0.0 $iface
+	done
+fi
 exit 0
