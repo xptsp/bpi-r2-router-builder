49b2a952d513a6d57432af0f51b8043a9a696442
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2022-04-13 13:37:35 -0500

    Updated WebUI Management / Attached Devices

--- router/includes/manage-attached.php
+++ router/includes/manage-attached.php
@@ -43,41 +43,45 @@ foreach (get_network_adapters() as $iface => $bridged)
 	if (!preg_match('/address\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.)/', @file_get_contents("/etc/network/interfaces.d/" . $iface), $regex))
 		continue;
 	$subnet = $regex[1];
-	$s = '
-                <table class="table table-hover text-nowrap">
-					<thead>
-						<tr>
-							<th style="width: 10px">#</th>
-							<th>MAC Address</th>
-							<th>Device Name</th>
-							<th>IP Address</th>
-							<th width="5%">WOL</th>
-						</tr>
-					</thead>
-					<tbody>';
-	$count = 0;
+	$s =
+                '<table class="table table-hover text-nowrap">' .
+					'<thead>' .
+						'<tr>' .
+							'<th style="width: 10px">#</th>' .
+							'<th>MAC Address</th>' .
+							'<th>Device Name</th>' .
+							'<th>IP Address</th>' .
+							'<th width="5%">WOL</th>' .
+						'</tr>' .
+					'</thead>' .
+					'<tbody>';
+	$dev = array();
 	foreach ($leases as $lease)
 	{
 		$parts = explode(" ", $lease);
 		if (substr($parts[2], 0, strlen($subnet)) == $subnet)
-			$s .= '
-						<tr>
-							<td>' . ++$count . '</td>
-							<td class="mac_addr">' . strtoupper($parts[1]) . '</td>
-							<td>' . $parts[3] . '</td>
-							<td>' . $parts[2] . '</td>
-							<td><a href="javascript:void(0);"><button type="button" class="btn btn-block btn-primary btn-xs"><i class="fas fa-power-off"></i></button></a></td>
-						</tr>';
+			$dev[ $parts[2] ] =
+						'<tr>' .
+							'<td>{count}</td>' .
+							'<td class="mac_addr">' . strtoupper($parts[1]) . '</td>' .
+							'<td>' . $parts[3] . '</td>' .
+							'<td>' . $parts[2] . '</td>' .
+							'<td><a href="javascript:void(0);"><button type="button" class="btn btn-block btn-primary btn-xs"><i class="fas fa-power-off"></i></button></a></td>' .
+						'</tr>';
 	}
-	if ($count == 0)
-		$s .= '
-						<tr>
-							<td colspan="5" class="centered"><h4>No Devices Attached</h4></td>
-						</tr>';
-	$s .= '
-					</tbody>
-				</table>';
-	if ($count > 0)
+	if (count($dev) == 0)
+		$s .= 
+						'<tr>' .
+							'<td colspan="5" class="centered"><h4>No Devices Attached</h4></td>' .
+						'</tr>';
+	ksort($dev);
+	$count = 0;
+	foreach ($dev as $id => $elem)
+		$dev[$id] = str_replace("{count}", strval(++$count), $elem);
+	$s .= implode("", $dev) .
+					'</tbody>' .
+				'</table>';
+	if (count($dev) > 0)
 		$table[$iface] = $s;
 }
 
