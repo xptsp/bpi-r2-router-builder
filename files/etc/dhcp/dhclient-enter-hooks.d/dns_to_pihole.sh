976cd6f7938353400127b30655e983e50d7c0b93
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2022-07-20 12:20:55 -0500

    Update dns_to_pihole.sh

--- files/etc/dhcp/dhclient-enter-hooks.d/dns_to_pihole.sh
+++ files/etc/dhcp/dhclient-enter-hooks.d/dns_to_pihole.sh
@@ -3,5 +3,5 @@
 # Override the function handling DNS addresses so that the updates 
 # happen in the PiHole configuration, not the "/etc/resolv.conf" file:
 make_resolv_conf() {
-	grep "use_isp=Y" /etc/default/router-settings >& /dev/null && router-helper dns ${new_domain_name_servers}
+	grep "use_isp=Y" /etc/default/router-settings >& /dev/null && echo "${new_domain_name_servers}" | sed "s| |\n|g" | sed "s|^|nameserver |g" > /etc/resolv.conf
 }
