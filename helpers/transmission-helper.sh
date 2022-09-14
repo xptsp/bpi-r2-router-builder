5ba34b6958a6f100c407304d863d1030c5f822dc
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2022-09-14 07:07:21 -0500

    Update transmission-helper.sh

--- helpers/transmission-helper.sh
+++ helpers/transmission-helper.sh
@@ -1,43 +1,49 @@
 #!/bin/bash
 #############################################################################
-# This helper script takes care of any tasks that should occur before the
-# transmission service officially starts.  Tasks that occur here should not
+# This helper script takes care of any tasks that should occur before 
+# launching the transmission-daemon program.  Tasks that occur here should not
 # take very long to execute and should not rely on other services being up
 # and running.
 #############################################################################
+
 #############################################################################
 # Our global variables:
 #############################################################################
 TABLE=$(grep -m 1 "^table inet " /etc/nftables.conf | awk '{print $3}')
 SETTINGS=/etc/transmission-daemon/settings.json
+test -f /etc/default/transmission-daemon && source /etc/default/transmission-daemon
 
 #############################################################################
 # Are we starting the service?
 #############################################################################
 if [[ "$1" == "start" ]]; then
-	# Add a firewall rule allowing inbound traffic over the VPN client:
-	if [[ "${UID}" -eq 0 ]]; then
-		IFACE=$(nft list set inet ${TABLE} DEV_VPN_CLIENT | grep "elements" | cut -d\" -f 2)
-		PEER=$(grep '"peer-port": [0-9]*' ${SETTINGS} | awk '{print $2}' | sed "s|,||")
-		nft add rule inet ${TABLE} input_vpn_client tcp dport ${PEER:-"51543"} accept comment "transmission-daemon"
-		exit $?
+	# Get IPv4 and IPv6 address from the VPN client interface to bind to.
+	IFACE=$(sudo $0 init | grep "^VPN=" | cut -d= -f 2) 
+	if [[ ! -z "${IFACE}" ]]; then
+		BIND_IPv4=$(ip addr show ${IFACE} | grep -m 1 inet | awk '{print $2}' | cut -d/ -f 1)
+		BIND_IPv6=$(ip addr show ${IFACE} | grep -m 1 inet6 | awk '{print $2}' | cut -d/ -f 1)
 	fi
-	sudo $0 start
-
-	# Set IPv4 and IPv6 addresses to bind to:
-	BIND_IPv4=$(ip addr show ${IFACE:-"lo"} | grep -m 1 inet | awk '{print $2}' | cut -d/ -f 1)
-	BIND_IPv4=${BIND_IPv4:-"255.255.255.1"}
-	sed -i "s|\"bind-address-ipv4\": \".*|\"bind-address-ipv4\": \"${BIND_IPv4}\",|" ${SETTINGS}
-	BIND_IPv6=$(ip addr show ${IFACE:-"lo"} | grep -m 1 inet6 | awk '{print $2}' | cut -d/ -f 1)
-	BIND_IPv6=${BIND_IPv6:-"fe80::"}
-	sed -i "s|\"bind-address-ipv6\": \".*|\"bind-address-ipv6\": \"${BIND_IPv6}\",|" ${SETTINGS}
+	sed -i "s|\"bind-address-ipv4\": \".*|\"bind-address-ipv4\": \"${IPs[0]:-"255.255.255.1"}\",|" ${SETTINGS}
+	sed -i "s|\"bind-address-ipv6\": \".*|\"bind-address-ipv6\": \"${IPs[1]:-"fe80::"}\",|" ${SETTINGS}
 
 	# Read transmission-daemon defaults and set credentials for WebUI:
 	# << Defaults >> Username: pi    Password: bananapi
-	test -f /etc/default/transmission-daemon && source /etc/default/transmission-daemon
 	sed -i "s|\"rpc-username\": \".*|\"rpc-username\": \"${TRANS_USER:-"pi"}\",|" ${SETTINGS}
 	sed -i "s|\"rpc-password\": \".*|\"rpc-password\": \"${TRANS_PASS:-"bananapi"}\",|" ${SETTINGS}
+	
+	# Start the daemon:
+	exec /usr/bin/transmission-daemon -f --log-error --bind-address-ipv4=${BIND_IPv4} --bind-address-ipv6=${BIND_IPv6}
 
+#############################################################################
+# Are we stopping the service?
+#############################################################################
+elif [[ "$1" == "stop" ]]; then
+	sudo $0 deinit
+
+#############################################################################
+# Do we need to change the WebUI and add a firewall rule?
+#############################################################################
+elif [[ "$1" == "init" ]]; then
 	# Change the transmission-daemon WebUI to choice in Router WebUI:
 	DIR=/usr/share/transmission
 	WEB=${DIR}/web
@@ -53,20 +59,20 @@ if [[ "$1" == "start" ]]; then
 		unlink ${WEB}
 		ln -sf ${DIR}/${TRANS_WEBUI} ${WEB}
 	fi
-	
-	# Start the daemon:
-	exec /usr/bin/transmission-daemon -f --log-error --bind-address-ipv4=${BIND_IPv4} --bind-address-ipv6=${BIND_IPv6}
+
+	# Add a firewall rule allowing inbound traffic over the VPN client:
+	IFACE=$(nft list set inet ${TABLE} DEV_VPN_CLIENT | grep "elements" | cut -d\" -f 2)
+	if [[ ! -z "${IFACE}" ]]; then
+		PEER=$(grep '"peer-port": [0-9]*' ${SETTINGS} | awk '{print $2}' | sed "s|,||")
+		nft add rule inet ${TABLE} input_vpn_client tcp dport ${PEER:-"51543"} accept comment "transmission-daemon"
+	fi
+	echo "VPN=$IFACE"
 
 #############################################################################
-# Otherwise, then we need to remove the firewall rule for VPN traffic:
+# Do we need to remove the firewall rule for VPN traffic?
 #############################################################################
-elif [[ "$1" == "stop" ]]; then
-	if [[ "${UID}" -ne 0 ]]; then
-		sudo $0 stop
-	else
-		HANDLE=$(nft -a list chain inet ${TABLE} input_vpn_client | grep "transmission-daemon" | awk '{print $NF}')
-		nft delete rule inet ${TABLE} input_vpn_client handle ${HANDLE}
-	fi
+elif [[ "$1" == "deinit" ]]; then
+	nft delete rule inet ${TABLE} input_vpn_client handle $(nft -a list chain inet ${TABLE} input_vpn_client | grep "transmission-daemon" | awk '{print $NF}')
 fi
 
 #############################################################################
