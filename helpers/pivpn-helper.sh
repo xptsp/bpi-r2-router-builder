7978cd6ac5b422d3e2a63156accd7ed5a91add00
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2022-11-11 07:39:31 -0600

    Updated PiVPN helper script and install script

--- helpers/pivpn-helper.sh
+++ helpers/pivpn-helper.sh
@@ -9,21 +9,29 @@ TXT="pivpn-openvpn"
 SKIP_MAIN=true
 
 #############################################################################################
+# Read in PiVPN variables:
+#############################################################################################
+# If the server name has been decided, read it in now.  This is done before
+# reading "setupVars.conf" to avoid incorrectly overwriting the setting.
+[[ -f /etc/openvpn/.server_name ]] && source /etc/openvpn/.server_name
+
+# Set all the variables:
+source /etc/pivpn/setupVars.conf
+
+#############################################################################################
 # Are we starting the service?  If so, do everything in this block:
 #############################################################################################
 if [[ "$1" == "start" ]]; then
+	# Make a copy of the settings files in temporary folder so we can modify them:
+	cp /etc/pivpn/openvpn/setupVars.conf /tmp/setupVars.conf
+
 	# Set variable "SKIP_MAIN" to "true" in order to skip execution of function "main" when sourcing the INSTALLER:
 	source /usr/local/src/modded_pivpn_install.sh
 
-	# If the server name has been decided, read it in now.  This is done before
-	# reading "setupVars.conf" to avoid incorrectly overwriting the setting.
-	[[ -f /etc/openvpn/.server_name ]] && source /etc/openvpn/.server_name
-
-	# Set all the variables:
-	source /etc/pivpn/setupVars.conf
-
 	# Determine IP address if one hasn't been specified already:
+	WRITE=false
 	if [ -z "${pivpnHOST}" ]; then
+		WRITE=true
 		if ! pivpnHOST=$(dig +short myip.opendns.com @resolver1.opendns.com); then
 			if ! pivpnHOST=$(curl eth0.me)
 			then
@@ -34,46 +42,43 @@ if [[ "$1" == "start" ]]; then
 	fi
 
 	# If certain settings aren't set, try to set them automagically:
-	[[ -z "${IPv4dev}" ]] && chooseInterface
-	[[ -z "${pivpnHOST}" ]] && askPublicIPOrDNS
-	[[ -z "${SERVER_NAME}" ]] && generateServerName
+	[[ -z "${IPv4dev}" ]] && WRITE=true && chooseInterface
+	[[ -z "${pivpnHOST}" ]] && WRITE=true && askPublicIPOrDNS
+	[[ -z "${SERVER_NAME}" ]] && WRITE=true && generateServerName
 
 	# Generate server certificate and DH parameters if necessary.
-	[[ ! -f /etc/openvpn/crl.pem ]] && GenerateOpenVPN
+	[[ ! -f /etc/openvpn/crl.pem ]] && WRITE=true && GenerateOpenVPN
 
 	# Create the "/etc/openvpn/server.conf" file if it doesn't already exist:
 	FILE=/etc/openvpn/pivpn.conf
 	if [[ ! -f ${FILE} ]]; then
-		createServerConf
+		WRITE=true && createServerConf
 		sed -i "s|dev tun|dev pivpn\ndev-type tun|" ${FILE}
 		echo "management 127.0.0.1 7505" >> ${FILE}
 	fi
 
 	# Configure OVPN if not already done so:
-	test -f /etc/openvpn/easy-rsa/pki/Default.txt || confOVPN
+	[[ ! -f /etc/openvpn/easy-rsa/pki/Default.txt ]] && WRITE=true && confOVPN
+
+	# Write altered PiVPN configuration back to storage location: 
+	[[ "${WRITE}" == "true" ]] && writeConfigFiles
 
 	# Remove any existing firewall rules for PiVPN: 
 	$0 stop
 
 	# Add the firewall rules to support PiVPN:
-	nft insert rule inet ${TABLE} input_wan ${pivpnPROTO,,} dport ${pivpnPORT} accept comment \"${TXT}\"
-	nft insert rule inet ${TABLE} forward iifname @DEV_WAN oifname ${pivpnDEV,,} ip daddr ${pivpnNET}/${subnetClass} ct state related,established accept comment \"${TXT}\"
-	nft insert rule inet ${TABLE} forward iifname ${pivpnDEV,,} oifname @DEV_WAN ip saddr ${pivpnNET}/${subnetClass} accept comment \"${TXT}\"
-
-	# Since PiVPN debug function cannot "see" the nftables masquerade rule, add an iptables rule for masquerade: 
-	iptables -t nat -A POSTROUTING -s "${pivpnNET}/${subnetClass}" -o "${IPv4dev}" -j MASQUERADE -m comment --comment "openvpn-nat-rule"
+	nft add rule inet ${TABLE} input_wan ${pivpnPROTO,,} dport ${pivpnPORT} accept comment \"${TXT}\"
+	nft add rule inet ${TABLE} forward iifname ${pivpnDEV,,} oifname @DEV_WAN ip saddr ${pivpnNET}/${subnetClass} accept comment \"${TXT}\"
+	nft add rule inet ${TABLE} nat_postrouting oifname @DEV_WAN ip saddr ${pivpnNET}/${subnetClass} masquerade comment \"${TXT}\"
 
 #############################################################################################
 # Are we stopping the service?  If so, remove the firewall rules:
 #############################################################################################
 elif [[ "$1" == "stop" ]]; then
 	# Remove any PiVPN nftables rules: 
-	for CHAIN in $(_nft list table inet ${TABLE} | grep chain | awk '{print $2}'); do
-		_nft -a list chain inet ${TABLE} ${CHAIN} | grep "${TXT}" | grep "handle" | awk '{print $NF}' | while read HANDLE; do
-			[[ "${HANDLE}" -gt 0 ]] 2> /dev/null && _nft delete rule inet ${TABLE} ${CHAIN} handle ${HANDLE}
+	for CHAIN in $(nft list table inet ${TABLE} | grep chain | awk '{print $2}'); do
+		nft -a list chain inet ${TABLE} ${CHAIN} | grep "${TXT}" | grep "handle" | awk '{print $NF}' | while read HANDLE; do
+			[[ "${HANDLE}" -gt 0 ]] 2> /dev/null && nft delete rule inet ${TABLE} ${CHAIN} handle ${HANDLE}
 		done
 	done
-	
-	# Remove the PiVPN iptables rule: 
-	iptables -t nat -D POSTROUTING -s "${pivpnNET}/${subnetClass}" -o "${IPv4dev}" -j MASQUERADE -m comment --comment "openvpn-nat-rule"
 fi
