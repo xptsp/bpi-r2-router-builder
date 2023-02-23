ad7fc7dc1bee42fe973d418847f9fc325f20923f
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2023-02-23 08:38:45 -0600

    Update smbd-helper.sh

--- helpers/smbd-helper.sh
+++ helpers/smbd-helper.sh
@@ -10,7 +10,8 @@
 FILE=/etc/samba/smb.conf
 cd /etc/network/interfaces.d
 IFACES=($(grep "address" $(grep -L "masquerade" *) | cut -d: -f 1))
-IFACES="${IFACES[@]}"
+IP_ADDR=$(ifconfig br0:1 | grep -m 1 "inet" | awk '{print $2}')/32
+IFACES="${IFACES[@]} ${IP_ADDR}"
 sed -i "s|interfaces = .*$|interfaces = ${IFACES}|" ${FILE}
 
 # Exit with error code 0:
